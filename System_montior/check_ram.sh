6a944ad777521b5a9eb9efb3cb37f44a811c191d
coohx <huangxin2028@gmail.com>
2016-08-21 00:24:27 +0800

    Monotor system

---
+++ System_montior/check_ram.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# The objective of this script is to check the Memory usage.
+# Author: huangxin Date:2016-08-20
+
+# maximum ratio of memory usage.
+mem_quota=80
+
+# fetch(提取) the ratio of memory usage.
+# return 1：if larger than $mem_quota
+#        0: if less than $mem_quota
+
+watch_memory()
+{
+	mem_total=`cat /proc/meminfo |grep MemTotal |awk '{print $2}'`
+	mem_free=`cat /proc/meminfo |grep MemFree |awk '{print $2}'`
+	#获取数值计算的结果: 用 $(())将运算公式扩起来
+	mem_usage=$((100 - mem_free*100/mem_total))
+	if [ $mem_usage -gt $mem_quota ];then
+		mem_message="ALLERM!!! The memory usage is $mem_usage%!!!"
+		return 1
+	else
+		return 0
+	fi
+}
+#watch_memory
